7442f541f78ce06df02547fe85a3834090509861
bitaim <54924193+bitaim@users.noreply.github.com>
2022-06-19 10:00:04 +0800

    Delete assets directory

--- assets/js/98.d36bef0a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[98],{298:function(t,e,n){"use strict";n.r(e);var s=n(0),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("高可用、高性能（吞吐量、响应时间）、高可靠")])])}),[],!1,null,null,null);e.default=l.exports}}]);
